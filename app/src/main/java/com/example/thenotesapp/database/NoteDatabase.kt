b511fb7c283e9a0331dc0e8fb2a68e2aafee12f1
TanishMoral11 <ishantmoral@gmail.com>
2024-07-30 22:52:05 +0530

    Add Comments

--- app/src/main/java/com/example/thenotesapp/database/NoteDatabase.kt
+++ app/src/main/java/com/example/thenotesapp/database/NoteDatabase.kt
@@ -1,4 +1,9 @@
 package com.example.thenotesapp.database
 
+import androidx.room.Database
+import com.example.thenotesapp.model.Note
+
+
+@Database(entities = [Note::class] , version = 1)
 class NoteDatabase {
 }
