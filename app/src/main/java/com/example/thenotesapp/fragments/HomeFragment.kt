6353bbd93663fcfc2233b675d67915548defc4a6
TanishMoral11 <ishantmoral@gmail.com>
2024-07-31 18:10:14 +0530

    Begin

--- app/src/main/java/com/example/thenotesapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/thenotesapp/fragments/HomeFragment.kt
@@ -1,102 +1,60 @@
 package com.example.thenotesapp.fragments
 
-import android.graphics.drawable.ClipDrawable.VERTICAL
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SearchView
-import androidx.core.view.MenuHost
-import androidx.core.view.MenuProvider
-import androidx.lifecycle.Lifecycle
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.example.thenotesapp.MainActivity
 import com.example.thenotesapp.R
-import com.example.thenotesapp.adapter.NoteAdapter
-import com.example.thenotesapp.model.Note
-import com.example.thenotesapp.viewmodel.NoteViewModel
 
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
-class HomeFragment : Fragment(R.layout.fragment_home) , SearchView.OnQueryTextListener, MenuProvider {
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
-    private var homeBinding: FragmentHomeBinding? = null
-    private val binding get() = homeBinding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
 
-    private lateinit var notesViewModel : NoteViewModel
-    private lateinit var noteAdapter: NoteAdapter
-
-        override fun onCreateView(
+    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-
         // Inflate the layout for this fragment
-       homeBinding  = FragmentHomeBinding.inflate(inflater, container, false)
-            return binding.root
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
-        super.onViewCreated(view, savedInstanceState)
-
-        val menuHost: MenuHost = requireActivity()
-        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
-
-        notesViewModel = (activity as MainActivity).noteViewModel
-
-        binding.addNoteFab.setOnClickListener{
-            it.findNavController()).navigate(R.id.action_homeFragent_to_addNoteFragment)        }
-    }
-
-    private fun updateUI(note : List<Note>?) {
-        if(note!=null){
-            if(note.isNotEmpty()){
-                binding.emptyNotesImage.visibility = View.GONE
-                binding.emptyRecyclerView.visibility = View.VISIBLE
-            }else{
-                binding.emptyNotesImage.visibility = View.VISIBLE
-                binding.emptyRecyclerview.visibility = View.GONE
-            }
-        }
-    }
-
-    private fun setupHomeRecyclerView() {
-        noteAdapter = noteAdapter()
-        binding.homeRecyclerView.apply {
-          layoutManager = StraggerdGridLayoutManager(2, StaggeredGridLayoutManager..VERTICAL)
-            setHasFixedSize(true)
-            adapter = noteAdapter
-
-        }
-
-        activity?.let {
-            notesViewModel.getAllNote().observe(viewLifecycleOwner){
-                noteAdapter.differ.submitList(note)
-                updateUI(note)
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment HomeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
             }
     }
-
-
-}
-
-    override fun onQueryTextSubmit(query: String?): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun onQueryTextChange(newText: String?): Boolean {
-        TODO("Not yet implemented")
-    }
-
-    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-
-    }
-
-    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-        TODO("Not yet implemented")
-    }
-
-
 }
